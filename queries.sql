a890c8eaeebb3352a7e39743cfb34ab491815ab9
erinaverill <averillerin@gmail.com>
2020-08-24 03:13:26 -0700

    updated queries

--- queries.sql
+++ queries.sql
@@ -1,22 +1,36 @@
 select 
 employees.emp_no,
-last_name,
-first_name,
-sex,
-salary
+last_name, first_name, sex, salary
 from employees
-inner join salaries on employees.emp_no=salaries.emp_no;
+inner join salaries on 
+employees.emp_no=salaries.emp_no;
 
-select last_name, first_name, hire_date from employees
+
+select 
+last_name, first_name, hire_date 
+from employees
 where (hire_date >= '1986-01-01' AND
 	  hire_date < '1987-01-01');
 
-select dept_manager.emp_no, departments.dept_name, dept_manager.dept_no,
-employees.last_name, 
-employees.first_name, dept_manager.from_date, dept_manager.to_date
-from employees inner join dept_manager on employees.emp_no=dept_manager.emp_no
+
+select 
+dept_manager.emp_no, departments.dept_name, 
+dept_manager.dept_no, employees.last_name, 
+employees.first_name, dept_manager.from_date, 
+dept_manager.to_date
+from employees 
+inner join dept_manager on employees.emp_no=dept_manager.emp_no
 inner join departments on departments.dept_no=dept_manager.dept_no;
 
 
 select * from employees
 where first_name = 'Hercules' and last_name like 'B%';
+
+
+select departments.dept_name, employees.emp_no, 
+employees.last_name, employees.first_name
+from employees
+inner join dept_manager on dept_manager.emp_no = employees.emp_no
+inner join departments on departments.dept_no = dept_manager.dept_no
+where dept_name = 'Sales' 
+or dept_name = 'Development';
